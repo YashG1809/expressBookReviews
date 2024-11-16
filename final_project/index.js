4baee592781ca2f38c4b230a5a6de239127c5e0c
YashG1809 <yashrajendragosavi111@gmail.com>
2024-11-16 07:28:43 -0500

    Your commit message

--- final_project/index.js
+++ final_project/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
@@ -12,6 +13,20 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
